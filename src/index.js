e9d8eb363ee9f0e5e1b856223df7efefb4661767
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-04-25 15:44:24 +0530

    Added CRUD operations

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const connect = require("./config/database");
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repo')
-
+const TweetRepository = require("./repository/tweet-repo");
 app.listen(3000, async () => {
   console.log("Listening on port 3000");
   await connect();
-  
+
   const tweetRepo = new TweetRepository();
-  try {
-    const tweet = await tweetRepo.create({ content: "First tweettt" });
-    console.log("Tweet created:", tweet);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+
+  //const tweet = await Tweet.find();//
+  //const tweet = await Tweet.create();//
+  const tweet = await tweetRepo.update(
+    "662a282641207eaf8e432147",
+    { userEmail:"b@a.com" },
+   
+  );
+  console.log("Tweet created:", tweet);
 });
--- src/repository/tweet-repo.js
+++ src/repository/tweet-repo.js
@@ -10,6 +10,30 @@ class TweetRepository {
       throw error; // Re-throw the error to handle it elsewhere
     }
   }
+  async get(id) {
+    try {
+      const tweet = await Tweet.findById(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async update(tweetId, data) {
+    try {
+      const tweet = await Tweet.findByIdAndUpdate(tweetId, data, { new: true });
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async destroy(id) {
+    try {
+      const tweet = await Tweet.findByIdAndRemove(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = TweetRepository;
