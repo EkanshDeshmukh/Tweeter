1fd853a222dbc817333e2706206a7dee33ef9acf
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-02 23:14:13 +0530

    Added Common CRUD Repository

---
+++ src/repository/crud-repo.js
@@ -0,0 +1,47 @@
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+  async create(data) {
+    try {
+      const result = await this.model.create(data);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in CRUD repo", error);
+    }
+  }
+  async destroy(id) {
+    try {
+      const result = await this.model.findByIdAndDelete(id);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in CRUD repo", error);
+    }
+  }
+  async update(id, data) {
+    try {
+      const result = await this.model.findByIdAndUpdate(id, data);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in CRUD repo", error);
+    }
+  }
+  async get(id) {
+    try {
+      const result = await this.model.findById(id);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in CRUD repo", error);
+    }
+  }
+  async getAll() {
+    try {
+      const result = await this.model.find();
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in CRUD repo", error);
+    }
+  }
+}
+
+export default CrudRepository;
