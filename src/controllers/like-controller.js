9f89b5d2e643131dcf1928e2d0d577bcc68e1578
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-04 23:33:35 +0530

    Added Like controller

---
+++ src/controllers/like-controller.js
@@ -0,0 +1,20 @@
+import LikeService from "../services/like-service.js";
+
+export const toggleLike = async (req, res) => {
+  try {
+    const response = await new LikeService().toggleLike(req.body);
+    return res.status(201).json({
+      success: true,
+      message: "Successfully toggled like",
+      data: response,
+      error: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong in like-controller",
+      data: {},
+      error: error,
+    });
+  }
+};
