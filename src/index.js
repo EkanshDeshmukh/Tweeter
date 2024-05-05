9f821b5a126b1e83802cb4a0f3cc2054cc284f8e
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-05 17:36:53 +0530

    Added Toggle like Api

--- src/index.js
+++ src/index.js
@@ -16,10 +16,10 @@ app.listen(3000, async () => {
   await connect();
   console.log("Connected to MongoDB !");
 
-  const userRepo = new UserRepository();
-  const tweetRepo = new TweetRepository();
+ // ** const userRepo = new UserRepository();
+  /* const tweetRepo = new TweetRepository();
   const tweets = await tweetRepo.getAll(0, 10);
   const users = await userRepo.getAll();
   const likeService = new LikeService();
-  await likeService.toggleLike(tweets[1].id, 'Tweet', users[1].id);
+  await likeService.toggleLike(tweets[1].id, 'Tweet', users[1].id);  **/
 });
